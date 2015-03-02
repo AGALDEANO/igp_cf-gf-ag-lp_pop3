3332b7947eeb07bf308f6f667a2b7848a6aff6f6
agaldeano <alexandre.galdeano@etu.univ-lyon1.fr>
2015-03-02 10:00:44 +0100

    client : ajout receive + convertisseur

--- client/src/util/ServerUtil.java
+++ client/src/util/ServerUtil.java
@@ -5,17 +5,42 @@ import base.Server;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
 /**
  * Created by alexandreg on 02/03/2015.
  */
 public class ServerUtil {
+    private static ServerUtil instance;
     private Server server;
     private OutputStream out;
     private InputStream in;
 
-    public ServerUtil(Server server) {
+    private ServerUtil(Server server) {
         this.server = server;
+        try {
+            this.out = server.getSocket().getOutputStream();
+            this.in = server.getSocket().getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ServerUtil getInstance(Server server) {
+        if (instance == null) {
+            instance = new ServerUtil(server);
+        }
+        return instance;
+    }
+
+    public static String bytesToAsciiString(byte[] data) {
+        try {
+            return new String(data, "ascii");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public void send(byte[] data) throws IOException {
@@ -23,8 +48,21 @@ public class ServerUtil {
         server.getSocket().getOutputStream().flush();
     }
 
-    public byte[] receive() throws IOException {
+    public void send(String str) throws IOException {
+        send(str.getBytes());
+    }
 
-        return null;
+    public byte[] receive() throws IOException {
+        ArrayList<Byte> bytes = new ArrayList<Byte>();
+        int i;
+        i = in.read();
+        while (i != -1) {
+            bytes.add((byte) i);
+        }
+        byte[] array = new byte[bytes.size()];
+        for (int j = 0; j < array.length; j++) {
+            array[j] = bytes.get(j);
+        }
+        return array;
     }
 }
