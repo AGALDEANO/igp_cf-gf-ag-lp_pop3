b57a81a313e4fd618ed5cd51d6bffccb28e7696d
firethorn <firethorn@firethorn-PC>
2015-03-02 10:09:16 +0100

    Pop3 commit

---
+++ client/src/util/Pop3Util.java
@@ -0,0 +1,17 @@
+package util;
+
+public class Pop3Util
+{
+	public static void sendAPOP(String username,String password)
+	{
+		
+	}
+	public static void sendRETR(int numeroMessage)
+	{
+		
+	}
+	public static void sendQUIT()
+	{
+		
+	}
+}
