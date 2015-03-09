5e64bba0a628627976f44f568ab4a77c071f5cda
agaldeano <alexandre.galdeano@etu.univ-lyon1.fr>
2015-03-09 13:41:36 +0100

    Email : ajout d'une fonction bodyToString() et toString()

--- client/src/base/Email.java
+++ client/src/base/Email.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * Created by alexandreg on 09/03/2015.
  */
 public class Email {
-    private static String endline = "\r\n";
+    private static String endLine = "\r\n";
     private static String endHeader = "\r\n\r\n";
     private static String headerSeparator = ": ";
     private static String endFile = "\r\n.\r\n";
@@ -16,7 +16,7 @@ public class Email {
 
     public Email(String response) {
         String[] splittedResponse = response.split(endHeader);
-        String[] strHeader = splittedResponse[0].split(endline);
+        String[] strHeader = splittedResponse[0].split(endLine);
         body = splittedResponse[1].split(endFile)[0];
         for (String header : strHeader) {
             headers.put(header.split(headerSeparator)[0], header.split(headerSeparator)[1]);
@@ -25,7 +25,13 @@ public class Email {
 
     public Email(Email e) {
         body = e.getBody();
-        headers = (HashMap<String, String>) e.getHeaders().clone();
+        Object clone = e.getHeaders().clone();
+        if (clone instanceof HashMap<?, ?>) {
+            HashMap hashMap = (HashMap) clone;
+            headers = hashMap;
+        } else {
+            headers = new HashMap<String, String>();
+        }
     }
 
     public HashMap<String, String> getHeaders() {
@@ -41,11 +47,18 @@ public class Email {
         for (Map.Entry<String, String> entry : headers.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
-            str += key + headerSeparator + endline;
+            str += key + headerSeparator + value + endLine;
         }
-        str += endline;
-        str += body;
 
         return str;
+    }
+
+    public String bodyToString() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        return headersToString() + endLine + bodyToString();
     }
 }
